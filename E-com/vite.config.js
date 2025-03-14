55df2045d93369b91dc962a94c5c21a7a727881e
Abimanyu C <131241129+Abimanyu700@users.noreply.github.com>
2025-03-14 18:40:11 +0530

    Update vite.config.js

--- E-com/vite.config.js
+++ E-com/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/E-com/", // Ensure GitHub Pages serves files from the correct path
+    base: "/E-com", 
   plugins: [react()],
 })
